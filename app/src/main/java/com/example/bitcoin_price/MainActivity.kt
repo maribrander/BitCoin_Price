690968dd6d76559e71844c11cb1c119813dce93f
maribrander <maribrander@yahoo.com.br>
2025-03-06 02:46:08 -0300

    Splash screen light mode Homescreen adicionada

--- app/src/main/java/com/example/bitcoin_price/MainActivity.kt
+++ app/src/main/java/com/example/bitcoin_price/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.bitcoin_price
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Handler (Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, Homescreen::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
     }
 }
